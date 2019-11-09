9458f12c7d4c3486b3cb4d331714ebea7243a225
linapacheco <lipamo08@gmail.com>
2019-11-09 11:56:29 -0500

    1 file modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MenuComponent } from './menu/menu.component';
+
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RoutingModule ],
+  declarations: [ AppComponent, MenuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
